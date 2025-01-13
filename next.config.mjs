a1714b18980c42fa111c53bea05a87f2f772537e
Ujjwal Kumar <ujjwal509kumar@gmail.com>
2025-01-13 15:35:50 +0530

    minor changes

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
           hostname: "images.unsplash.com", 
           pathname: "/**",                
         },
+        {
+          protocol: "https",
+          hostname: "lh3.googleusercontent.com", 
+          pathname: "/**",               
+        },
       ],
     },
   };
