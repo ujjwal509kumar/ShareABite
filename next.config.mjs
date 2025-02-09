5593eb8d43f40044f62b41bf02b5679191092ebd
Ujjwal Kumar <ujjwal509kumar@gmail.com>
2025-02-09 10:52:05 +0530

    added freepik domain

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,11 @@ const nextConfig = {
           hostname: "lh3.googleusercontent.com", 
           pathname: "/**",               
         },
+        {
+          protocol: "https",
+          hostname: "img.freepik.com",
+          pathname: "/**",
+        }
       ],
     },
   };
