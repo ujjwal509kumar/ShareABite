8835f649dfff43b4a0038c3b2e4a3cd556726c48
Ujjwal Kumar <ujjwal509kumar@gmail.com>
2025-02-11 09:31:22 +0530

    trying ti make it dynamic

--- app/api/getactivecenter/route.js
+++ app/api/getactivecenter/route.js
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 const prisma = new PrismaClient();
 
 export async function GET() {
