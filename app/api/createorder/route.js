5c450ac78ec4f72a2d5f8323d3cdb8f4c419e5ce
Ujjwal Kumar <ujjwal509kumar@gmail.com>
2025-05-25 20:20:12 +0530

    fixed a small payment related bug

--- app/api/createorder/route.js
+++ app/api/createorder/route.js
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 import Razorpay from 'razorpay';
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+  key_secret: process.env.NEXT_PUBLIC_RAZORPAY_KEY_SECRET,
 });
 
 export async function POST(request) {
